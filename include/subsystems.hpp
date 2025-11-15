80390a190871d10affa56fb38e30fe7517e08271
Jimmy <junycaiu@gmail.com>
2025-11-14 22:05:13 -0800

    init commit

---
+++ include/subsystems.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "EZ-Template/api.hpp"
+#include "api.h"
+
+extern Drive chassis;
+
+// Your motors, sensors, etc. should go here.  Below are examples
+
+// inline pros::Motor intake(1);
+// inline pros::adi::DigitalIn limit_switch('A');
+inline pros::Motor intake_motor(7);
+inline ez::Piston piston_wing('A');
+inline ez::Piston sled('B');
